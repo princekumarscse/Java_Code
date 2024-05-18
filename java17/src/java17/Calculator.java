0a3db97dc72cda957e531adeb66476abce6d8698
princekumarsesc <pk8849682@gmail.com>
2024-05-18 11:44:58 +0530

    calculator

--- java17/src/java17/Calculator.java
+++ java17/src/java17/Calculator.java
@@ -1,30 +1,28 @@
 package java17;
 
 public class Calculator {
-	 public int addition(int a,int b) {
+	public int addition(int a, int b) {
 		return a + b;
 	}
-	 public int subs(int a, int b)
-	 {
-		 return a - b;
-	 }
-	 public int multi(int a, int b)
-	 {
-		 return a * b;
-	 }
-	 public int div(int a, int b)
-	 {
-		 return a / b;
-	 }
-	 
-	 
+
+	public int subs(int a, int b) {
+		return a - b;
+	}
+
+	public int multi(int a, int b) {
+		return a * b;
+	}
+
+	public int div(int a, int b) {
+		return a / b;
+	}
 
 	public static void main(String[] args) {
-		
-		Calculator cal =new Calculator();
-		
+
+		Calculator cal = new Calculator();
+
 		int ad = cal.addition(4, 5);
-		System.out.println("Addition = " +ad );
+		System.out.println("Addition = " + ad);
 	}
 
 }
