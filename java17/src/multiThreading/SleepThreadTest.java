5fc0de6365b1425b4e08925e6a8bcca3c9cf26bb
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-04-07 20:51:59 +0530

    Update SleepThreadTest.java

--- java17/src/multiThreading/SleepThreadTest.java
+++ java17/src/multiThreading/SleepThreadTest.java
@@ -3,7 +3,7 @@ package multiThreading;
 public class SleepThreadTest {
 	public static void main(String[] args) {
 		SleepThread sleepThread = new SleepThread();
-		sleepThread.start();
+		sleepThread.start();  
 		for (int i = 1; i <= 5; i++) {
 			System.out.println("I am main thread");
 		}
