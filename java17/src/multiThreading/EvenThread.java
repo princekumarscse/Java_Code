d2927134aa4b78249d5aea27872d83967d961767
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-04-10 18:30:10 +0530

    Update EvenThread.java

--- java17/src/multiThreading/EvenThread.java
+++ java17/src/multiThreading/EvenThread.java
@@ -7,7 +7,7 @@ public void run() {
 //for(int i=1;i<=10;i++) {
 //	if(i%2==0) {
 //		//System.out.println(Thread.currentThread().getName()+" "+i);
-//	}
+//	} 
 //}
 //System.out.println(Thread.currentThread().getName());
 ThreadHelper.sortStudent();
