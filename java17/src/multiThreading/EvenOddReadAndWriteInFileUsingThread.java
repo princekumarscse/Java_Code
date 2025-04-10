bce72026f67458fc207207ddf605afaf2a45f522
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-04-10 18:29:52 +0530

    Update EvenOddReadAndWriteInFileUsingThread.java

--- java17/src/multiThreading/EvenOddReadAndWriteInFileUsingThread.java
+++ java17/src/multiThreading/EvenOddReadAndWriteInFileUsingThread.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.List; 
+import java.util.List;  
 
 public class EvenOddReadAndWriteInFileUsingThread {
 public static void main(String[] args) throws IOException {
