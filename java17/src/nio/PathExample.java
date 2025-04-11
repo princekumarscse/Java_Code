bcd54a7d8219e7246977eb9d82a74e6af837ab05
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-04-11 18:30:56 +0530

    Update PathExample.java

--- java17/src/nio/PathExample.java
+++ java17/src/nio/PathExample.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	Path path=Paths.get("C:\\Users\\TWINKLE THAKUR\\git\\repository\\java_8");
 	System.out.println(path.getRoot());
 	System.out.println(path.getParent());
-	System.out.println(path.getFileName());
+	System.out.println(path.getFileName()); 
 	System.out.println(path.getFileSystem());
 }
 }
