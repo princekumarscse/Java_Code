85e0d8666b687bd7e3254842a0673ce5bb718b9c
princekumarscse <pk88498650@gmail.com>
2025-04-18 22:27:44 +0530

    commit

---
+++ java17/src/advanceThreadingConcept/ExecutorExample.java
@@ -0,0 +1,23 @@
+package advanceThreadingConcept;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorExample {
+public static void main(String[] args) {
+	ExecutorService service = Executors.newFixedThreadPool(2);
+	Runnable oddThread=()->{
+		for(int i=1;i<=10;i++) {
+			System.out.println("I am odd Thread");
+		}
+	};
+	Runnable evenThread=()->{
+		for(int i=1;i<=10;i++) {
+			System.out.println("I am even Thread");
+		}
+	};
+	service.submit(evenThread);
+	service.submit(oddThread);
+	service.shutdown();
+}
+}
