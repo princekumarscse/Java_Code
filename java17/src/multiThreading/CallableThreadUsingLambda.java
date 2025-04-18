27a7b125cb75b852f59059e2d6eb4f8e7e80c807
princekumarscse <pk88498650@gmail.com>
2025-04-18 22:38:27 +0530

    update 1.0

--- java17/src/multiThreading/CallableThreadUsingLambda.java
+++ java17/src/multiThreading/CallableThreadUsingLambda.java
@@ -1,6 +1,9 @@
 package multiThreading;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CallableThreadUsingLambda {
 public static void main(String[] args) throws Exception {
@@ -10,9 +13,12 @@ public static void main(String[] args) throws Exception {
 		int sum=0;
 		for(int i=1;i<=10;i++) {
 			sum+=i;
-		}  
+		}
 		return sum;
 	};
-	
+	ExecutorService service = Executors.newFixedThreadPool(1);
+	Future<Integer> future = service.submit(callableThread);
+	System.out.println(future.get());
+	service.shutdown();
 }
 }
