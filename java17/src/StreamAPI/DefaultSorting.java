269464f10680764c01c78b19c724e9f1607b642e
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-04-22 20:20:18 +0530

    Update DefaultSorting.java

--- java17/src/StreamAPI/DefaultSorting.java
+++ java17/src/StreamAPI/DefaultSorting.java
@@ -11,7 +11,7 @@ public static void main(String[] args) {
 	List<Integer> list = new ArrayList<>();
 	list.add(40);
 	list.add(5);
-	list.add(50);
+	list.add(50); 
 	list.add(30);  
 	list.add(20);
 	//Collections.sort(list);
