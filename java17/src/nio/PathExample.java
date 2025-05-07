670e6df2352ca94a4d7311d81ebe93d32a45ef15
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-07 21:11:08 +0530

    Update PathExample.java

--- java17/src/nio/PathExample.java
+++ java17/src/nio/PathExample.java
@@ -9,6 +9,6 @@ public static void main(String[] args) {
 	System.out.println(path.getRoot());
 	System.out.println(path.getParent());
 	System.out.println(path.getFileName()); 
-	System.out.println(path.getFileSystem());
+	System.out.println(path.getFileSystem());  
 }
 }
