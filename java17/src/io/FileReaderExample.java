50600c5fc785d243e816d1dbf5c491e34558d590
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-15 07:28:11 +0530

    Update FileReaderExample.java

--- java17/src/io/FileReaderExample.java
+++ java17/src/io/FileReaderExample.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+ 
 public class FileReaderExample {
 public static void main(String[] args) {
 	File file=new File("myTest.txt");
