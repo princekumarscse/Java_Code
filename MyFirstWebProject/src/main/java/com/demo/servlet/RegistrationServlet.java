8f01fbfa4e7c85d226a3f69c6d6805a59677c82b
princekumarscse <your_github_email@example.com>
2025-05-30 19:16:34 +0530

    commit 1.1
    # Conflicts:
    #	MyFirstWebProject/src/main/webapp/login.html

---
+++ MyFirstWebProject/src/main/java/com/demo/servlet/RegistrationServlet.java
@@ -0,0 +1,55 @@
+package com.demo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/registration.do")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RegistrationServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("username");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String gender = request.getParameter("gender");
+		String course = request.getParameter("course");
+		String[] courses = request.getParameterValues("course");
+		String timing = request.getParameter("timing");
+		System.out.println(name + " " + email + " " + phone + " " + gender);
+		System.out.println(course);
+		System.out.println(timing);
+		Arrays.asList(courses).stream().forEach(System.out::println);
+//send response to browser
+		PrintWriter writer = response.getWriter();
+		String message = "<html><body><ul>" + "<li>" + name + "</li>" + "<li>" + email + "</li>" + "<li>" + phone
+				+ "</li>" + "<li>" + gender + "</li>" + "<li>" + course + "</li>" + "<li>" + timing + "</li>"
+				+ "</ul></body></html>";
+		writer.write(message);
+		
+
+	}
+
+}
