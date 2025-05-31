6c2d3e732093e2b724b078a85a4c512d8232700c
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-31 21:28:10 +0530

    Update RegistrationServlet.java

--- MyFirstWebProject/src/main/java/com/demo/servlet/RegistrationServlet.java
+++ MyFirstWebProject/src/main/java/com/demo/servlet/RegistrationServlet.java
@@ -3,7 +3,7 @@ package com.demo.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
-
+ 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
