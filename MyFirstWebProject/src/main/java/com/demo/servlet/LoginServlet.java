54c5ee2b01abfb5e6f608459ee73af6e696ddbe2
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-31 21:27:47 +0530

    Update LoginServlet.java

--- MyFirstWebProject/src/main/java/com/demo/servlet/LoginServlet.java
+++ MyFirstWebProject/src/main/java/com/demo/servlet/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.demo.jdbcUtil.UserService;
-
+ 
 /**
  * Servlet implementation class LoginServlet
  */
