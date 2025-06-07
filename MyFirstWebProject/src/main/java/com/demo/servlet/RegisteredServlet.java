99f8d612dc6c44a1a4bcc31c0c4bbebf8284a0f8
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-07 23:27:58 +0530

    Update RegisteredServlet.java

--- MyFirstWebProject/src/main/java/com/demo/servlet/RegisteredServlet.java
+++ MyFirstWebProject/src/main/java/com/demo/servlet/RegisteredServlet.java
@@ -1,7 +1,7 @@
 package com.demo.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.PrintWriter;  
 import java.util.Arrays;
 
 import javax.servlet.ServletException;
