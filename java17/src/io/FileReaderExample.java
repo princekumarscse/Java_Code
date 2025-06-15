31fa8227fa12eeb9f2970133551e76e01621b9c0
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-15 19:29:00 +0530

    Update FileReaderExample.java

--- java17/src/io/FileReaderExample.java
+++ java17/src/io/FileReaderExample.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class FileReaderExample {
 public static void main(String[] args) {
 	File file=new File("myTest.txt");
-	FileReader fr=null;
+	FileReader fr=null; 
 	try {
 		 fr=new FileReader(file);
 		
