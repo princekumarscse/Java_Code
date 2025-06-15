3159415534a2231f6ea6a127cc25647a0f359a58
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-15 19:28:11 +0530

    Update ConsoleReader.java

--- java17/src/io/ConsoleReader.java
+++ java17/src/io/ConsoleReader.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.StringTokenizer; 
 import java.util.stream.Collectors; 
 
 public class ConsoleReader {
