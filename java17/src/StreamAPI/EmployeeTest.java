1015c7a2a5684627cf7802aec377b67e080a2719
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-08-18 00:37:37 +0530

    Update EmployeeTest.java

--- java17/src/StreamAPI/EmployeeTest.java
+++ java17/src/StreamAPI/EmployeeTest.java
@@ -8,7 +8,7 @@ package StreamAPI;
  
 import java.util.Comparator;
 import java.util.List; 
-import java.util.function.Function;
+import java.util.function.Function; 
 import java.util.function.Predicate; 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
