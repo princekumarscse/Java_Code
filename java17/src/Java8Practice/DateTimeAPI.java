f8ec00c2b45dbbd7d1db342db7208ddc078f3d9b
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-09-16 00:10:13 +0530

    Update DateTimeAPI.java

--- java17/src/Java8Practice/DateTimeAPI.java
+++ java17/src/Java8Practice/DateTimeAPI.java
@@ -6,7 +6,7 @@ public class DateTimeAPI {
 public static void main(String[] args) {
 	LocalDate dob = LocalDate.of(2000, 03, 22);
 	LocalDate now = LocalDate.now();
-	 
+	  
 	LocalDate age = now.minusYears(dob.getYear());
 	System.out.println(age.getYear());
 	
