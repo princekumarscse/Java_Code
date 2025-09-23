18d4960d0abf769fc6a493724098c5468b38f898
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-09-23 21:44:56 +0530

    Update StudentClient.java

--- java17/src/Encapsulation/StudentClient.java
+++ java17/src/Encapsulation/StudentClient.java
@@ -2,7 +2,7 @@ package Encapsulation;
 
 public class StudentClient {
 	public static void main(String[] args) {
-		Student  std= new Student(); 
+		Student  std= new Student();  
 		std.setRollNumber(14);
 		System.out.println(std.getRollNumber());
 		System.out.println(std.getAge());
