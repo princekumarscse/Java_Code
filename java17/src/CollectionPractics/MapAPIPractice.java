2b7d11e646062d0616705b22b11f58b008ef1990
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-09-25 21:34:07 +0530

    Update MapAPIPractice.java

--- java17/src/CollectionPractics/MapAPIPractice.java
+++ java17/src/CollectionPractics/MapAPIPractice.java
@@ -1,7 +1,7 @@
 package CollectionPractics; 
  
 import java.util.HashMap;
-import java.util.Iterator;    
+import java.util.Iterator;     
 import java.util.Map;      
 import java.util.Map.Entry;     
 import java.util.function.BiConsumer;    
