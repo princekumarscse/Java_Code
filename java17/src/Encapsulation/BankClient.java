33ad1f4cd738d5413eb961b1809f855be84e6c6f
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-10-30 00:16:11 +0530

    Update BankClient.java

--- java17/src/Encapsulation/BankClient.java
+++ java17/src/Encapsulation/BankClient.java
@@ -1,6 +1,6 @@
 package Encapsulation;
 
-public class BankClient {
+public class BankClient { 
 	public static void main(String[] args) {
 		Bank bank = new Bank();
 		System.out.println(bank.withdraw(5000));  
